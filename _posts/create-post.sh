07094f331261f32799960e363de6cefcc66f6c97
eoliver <eo.revolver@gmail.com>
2020-02-09 18:02:25 -0800

    Testing a new post.

--- _posts/create-post.sh
+++ _posts/create-post.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d`
-post_name=$1-$DATE.md
+post_name=$DATE-$1.md
 
 cat <<EOT >> $post_name
 ---
 layout: post
-author: serverless
-series:
-part:
+title: "$1"
+date: $DATE
+categories: [AWS]
 ---
 EOT
 
